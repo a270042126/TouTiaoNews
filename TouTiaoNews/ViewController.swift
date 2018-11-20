d98589ca6baf445e8d35861b2b5bf1e7bf9aca8e
a270042126 <270042126@qq.com>
2018-11-20 13:03:35 +0800

    Initialize the project

---
+++ TouTiaoNews/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TouTiaoNews
+//
+//  Created by dd on 2018/11/20.
+//  Copyright © 2018年 dd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ TouTiaoNewsTests/TouTiaoNewsTests.swift
@@ -0,0 +1,34 @@
+//
+//  TouTiaoNewsTests.swift
+//  TouTiaoNewsTests
+//
+//  Created by dd on 2018/11/20.
+//  Copyright © 2018年 dd. All rights reserved.
+//
+
+import XCTest
+@testable import TouTiaoNews
+
+class TouTiaoNewsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
